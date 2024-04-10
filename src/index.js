be94dcdf3be14c1507b391cf5fdbcd1fa739b9a8
Patrick Dolan <dolanp1992@gmail.com>
2024-04-10 14:22:18 -0700

    add multiple plant functionality

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './css/styles.css';
 
 const storeState = () => {
   let currentState = {};
-  return (stateChangeFunction = state => state) => {
-    const newState = stateChangeFunction(currentState);
-    currentState = {...newState};
-    return newState;
+  return () => {
+    const plantNum = Object.keys(currentState).length + 1;
+    currentState[plantNum] = {plant: plantNum};
+    return (stateChangeFunction = state => state) => {
+      const newState = stateChangeFunction(currentState[plantNum]);
+      currentState[plantNum] = {...newState};
+      console.log(currentState);
+      return newState;
+    };
   };
 };
 
@@ -20,6 +25,25 @@ const changeState = (prop) => {
   };
 };
 
+// UI Logic
+
+function createPlantCardElement(plantNum) {
+  const plantCard = document.createElement('div');
+  plantCard.className = 'card mt-3';
+  plantCard.innerHTML = `
+    <div class="card-body">
+      <h5 class="card-title">Plant ${plantNum}</h5>
+      <p class="card-text">Soil: <span id="${plantNum}-soil-value">0</span></p>
+      <button id="${plantNum}-feed" class="btn btn-primary">Feed</button>
+      <button id="${plantNum}-super-feed" class="btn btn-primary">Super Feed</button>
+      <p class="card-text">Hydration: <span id="${plantNum}-hydration-value">0</span></p>
+      <button id="${plantNum}-hydrate" class="btn btn-primary">Hydrate</button>
+      <button id="${plantNum}-super-hydrate" class="btn btn-primary">Super Hydrate</button>
+    </div>
+  `;
+  return plantCard;
+}
+
 window.onload = function() {
   // Overall State
   const stateControl = storeState();
@@ -31,23 +55,30 @@ window.onload = function() {
   const hydrate = changeState("water")(1);
   const superWater = changeState("water")(5);
 
-  // UI Logic
-  // - Soil Buttons
-  document.getElementById('feed').onclick = function() {
-    const newState = stateControl(feed);
-    document.getElementById('soil-value').innerText = `${newState.soil}`;
-  };
-  document.getElementById('super-feed').onclick = function() {
-    const newState = stateControl(blueFood);
-    document.getElementById('soil-value').innerText = `${newState.soil}`;
-  };
-  // - Water Buttons
-  document.getElementById('hydrate').onclick = function() {
-    const newState = stateControl(hydrate);
-    document.getElementById('hydration-value').innerText = `${newState.water}`;
-  };
-  document.getElementById('super-hydrate').onclick = function() {
-    const newState = stateControl(superWater);
-    document.getElementById('hydration-value').innerText = `${newState.water}`;
+  document.getElementById('create-plant').onclick = function() {
+    // Create new Plant and Plant Card
+    const newPlantStateControl = stateControl();
+    const plantNum = newPlantStateControl().plant;
+    const plantCard = createPlantCardElement(plantNum);
+    document.getElementById('plant-cards').appendChild(plantCard);
+
+    // - Soil Buttons
+    document.getElementById(`${plantNum}-feed`).onclick = function() {
+      const newState = newPlantStateControl(feed);
+      document.getElementById(`${plantNum}-soil-value`).innerText = `${newState.soil}`;
+    };
+    document.getElementById(`${plantNum}-super-feed`).onclick = function() {
+      const newState = newPlantStateControl(blueFood);
+      document.getElementById(`${plantNum}-soil-value`).innerText = `${newState.soil}`;
+    };
+    // - Water Buttons
+    document.getElementById(`${plantNum}-hydrate`).onclick = function() {
+      const newState = newPlantStateControl(hydrate);
+      document.getElementById(`${plantNum}-hydration-value`).innerText = `${newState.water}`;
+    };
+    document.getElementById(`${plantNum}-super-hydrate`).onclick = function() {
+      const newState = newPlantStateControl(superWater);
+      document.getElementById(`${plantNum}-hydration-value`).innerText = `${newState.water}`;
+    };
   };
 };
